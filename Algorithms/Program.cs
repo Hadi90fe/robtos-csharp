eed3373822889cd8118cb79b8e14ee0167f42589
didier amyot <didier@nimblerx.com>
2023-08-13 08:04:01 -0400

    added vscode settings.json

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,2 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Algorithms;
+using System;
+
+class Point{
+    
+    private double x;
+    private double y;
+    
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public double Distance(){
+        return Math.Sqrt(x * x + x * y);
+    }
+}
+
+
+class Foo{
+    public static void Main(String[] args){
+        Point p = new Point(3, 4);
+        Console.WriteLine("Hello World From A Class: " + p.Distance());
+    }
+}
